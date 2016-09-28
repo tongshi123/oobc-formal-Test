5db308194f9844390ed9b39bbcf7fb2c22f0b483
xujing22 <xujing22@baidu.com>
2016-09-28 10:37:37 +0800

    refactor CMake to support C++11

--- src/order.cpp
+++ src/order.cpp
@@ -12,5 +12,6 @@ Status Order::fill(Warehouse & warehouse) {
 	if (warehouse.get_inventory(_item_name) < _item_count) {
 		return FAILED;
 	}
+	int && i = 1;
 	return warehouse.remove(_item_name, _item_count);
 }
