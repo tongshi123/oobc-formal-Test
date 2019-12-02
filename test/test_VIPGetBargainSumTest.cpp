a6095e57c7891eb2cd7ce34112be386396966bbe
jing.q.xu <jing.q.xu@hotmail.com>
2019-12-02 20:38:23 +0800

    Update test_VIPGetBargainSumTest.cpp

--- test/test_VIPGetBargainSumTest.cpp
+++ test/test_VIPGetBargainSumTest.cpp
@@ -8,10 +8,11 @@
 #include "ShoppingSystem.h"
 #include <gtest/gtest.h>
 
-
+//ShoppingSystem, 这个名字是不是有点太大了？
 TEST(VIPGetBargainSumTest, Bargain_Item_coffee_is_48_sould_payfor_48_without_VIP){
 	ShoppingSystem shopping;
 	shopping.buy(Coffee);
+	//getPayment或者getCost会更直白？
 	EXPECT_EQ(48, shopping.getSum());
 }
 
